930dd83e2e223492452b3e2d4aa5cfe25bcbdc3b
babaoui <73282716+babaoui@users.noreply.github.com>
2020-11-10 14:26:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let i = document.getElementById('i');
+let o = document.getElementById('o');
+let g = document.getElementById('g');
+
+g.addEventListener('click', () => {
+    o.innerHTML = i.value.match(/[^,]*(?=( *, *)|$)/g).map(x => x.split('').map(c => String.fromCharCode(c.charCodeAt()+1)).join('')).join('_').replace(/_+/g,'_')
+    o.innerHTML = o.innerHTML.substring(0, o.innerHTML.length - 1);
+});
