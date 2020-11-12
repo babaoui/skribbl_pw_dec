b0a11a99eaac9485d4bca33c6c56dac3da836b06
babaoui <73282716+babaoui@users.noreply.github.com>
2020-11-12 11:52:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let o = document.getElementById('o');
 let g = document.getElementById('g');
 
 g.addEventListener('click', () => {
-    o.innerHTML = i.value.match(/[^,]*(?=( *, *)|$)/g).map(x => x.split('').map(c => String.fromCharCode(c.charCodeAt()+1)).join('')).join('_').replace(/_+/g,'_')
+    o.innerHTML = i.value.match(/[^,]*(?=( *, *)|$)/g).map(x => x.split('').map(c => String.fromCharCode(c.charCodeAt()+1)).join('')).join('%%%').replace(/(%%%)+/g,'%%%')
     o.innerHTML = o.innerHTML.substring(0, o.innerHTML.length - 1);
 });
