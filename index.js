e772edcebbcaba6d4d2765140546842c1fed4f2b
babaoui <73282716+babaoui@users.noreply.github.com>
2020-11-12 11:54:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let g = document.getElementById('g');
 
 g.addEventListener('click', () => {
     o.innerHTML = i.value.match(/[^,]*(?=( *, *)|$)/g).map(x => x.split('').map(c => String.fromCharCode(c.charCodeAt()+1)).join('')).join('%%%').replace(/(%%%)+/g,'%%%')
-    o.innerHTML = o.innerHTML.substring(0, o.innerHTML.length - 1);
+    o.innerHTML = o.innerHTML.substring(0, o.innerHTML.length - 3);
 });
